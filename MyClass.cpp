d501686e1f82e1c0334d6796c2fe98d3c4e3c42d
ElinaIskhakova <elina_vinston@mail.ru>
2018-03-22 18:25:57 +0500

    Add method: setStroka

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,6 +1,15 @@
 #include "myclass.h"
-
+#include "string.h"
+#include "iostream"
+using namespace std;
 MyClass::MyClass()
 {
 
 }
+
+void MyClass::setStroka()
+ {
+
+      cin>>Stroka;
+
+ }
--- MyClass.h
+++ MyClass.h
@@ -1,11 +1,15 @@
 ifndef MYCLASS_H
 #define MYCLASS_H
-
+#include "string.h"
 
 class MyClass
 {
 public:
     MyClass();
+    void SetStroka();
+    void getStroka(); 
+private:
+	string Stroka;
 };
 
 #endif // MYCLASS_H
